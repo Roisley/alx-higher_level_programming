2b67792d6d82c22710b263d9bc6cbdc781d0dbe2
Roisley <Kingsleyezejide@gmail.com>
2023-01-03 12:14:52 -0800

    Python - Everything is object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
+"""
+Module for a class that prevents dynamic attributes creation
+
+"""
+
+
+class LockedClass():
+    """Class to prevent dynamic attributes creation"""
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """Init method"""
+        pass
